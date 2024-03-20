5a50eb317fbce8b1d585601c16f5da5d09c69723
Dominik Stadler <dominik.stadler@dynatrace.com>
2024-03-20 12:23:09 +0100

    Indicate in log how images are created

--- src/main/java/org/dstadler/ctw/tiles/CreateTileOverlaysHelper.java
+++ src/main/java/org/dstadler/ctw/tiles/CreateTileOverlaysHelper.java
@@ -222,7 +222,7 @@ public class CreateTileOverlaysHelper {
 				}
 
 				if (lastLog.get() + TimeUnit.SECONDS.toMillis(5) < System.currentTimeMillis()) {
-					log.info(String.format(Locale.US, "zoom %d: %,d of %,d: %s%s",
+					log.info(String.format(Locale.US, "bool[] -> png: zoom %d: %,d of %,d: %s%s",
 							zoom, tileNr, tileCount, file, concatProgress()));
 
 					lastLog.set(System.currentTimeMillis());
@@ -324,7 +324,7 @@ public class CreateTileOverlaysHelper {
 		}
 
 		if (lastLog.get() + TimeUnit.SECONDS.toMillis(5) < System.currentTimeMillis()) {
-			log.info(String.format(Locale.US, "zoom %d: %,d of %,d: %s%s",
+			log.info(String.format(Locale.US, "features -> png: zoom %d: %,d of %,d: %s%s",
 					tile.getZoom(), tilesNr, tilesOut.size(), tile.toCoords(), CreateTileOverlaysHelper.concatProgress()));
 
 			lastLog.set(System.currentTimeMillis());
