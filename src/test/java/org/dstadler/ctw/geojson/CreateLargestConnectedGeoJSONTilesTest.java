ed49d02f20268b9e21012f45f6fa7b8c1a3dda46
Dominik Stadler <dominik.stadler@gmx.at>
2024-06-24 08:34:50 +0200

    Add simple tests for computing largest connected

---
+++ src/test/java/org/dstadler/ctw/geojson/CreateLargestConnectedGeoJSONTilesTest.java
@@ -0,0 +1,20 @@
+package org.dstadler.ctw.geojson;
+
+import java.io.IOException;
+
+import org.dstadler.commons.testing.PrivateConstructorCoverage;
+import org.junit.jupiter.api.Test;
+
+class CreateLargestConnectedGeoJSONTilesTest {
+	@Test
+	void test() throws IOException {
+		// for now simply run the application
+		CreateLargestConnectedGeoJSONTiles.main(new String[0]);
+	}
+
+	// helper method to get coverage of the unused constructor
+	@Test
+	void testPrivateConstructor() throws Exception {
+		PrivateConstructorCoverage.executePrivateConstructor(CreateLargestConnectedGeoJSONTiles.class);
+	}
+}
