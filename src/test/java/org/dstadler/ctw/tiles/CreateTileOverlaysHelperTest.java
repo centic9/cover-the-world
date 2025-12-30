06b4b3efb5828289aa570057cf81badd5740967f
Dominik Stadler <dominik.stadler@gmx.at>
2025-12-30 10:07:27 +0100

    Use a fixed local for one test

--- src/test/java/org/dstadler/ctw/tiles/CreateTileOverlaysHelperTest.java
+++ src/test/java/org/dstadler/ctw/tiles/CreateTileOverlaysHelperTest.java
@@ -2,38 +2,48 @@ package org.dstadler.ctw.tiles;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Locale;
+
 import org.junit.jupiter.api.Test;
 
 class CreateTileOverlaysHelperTest {
 
 	@Test
 	void concatProgress() {
-		CreateTileOverlaysHelper.ACTUAL.clear();
-		CreateTileOverlaysHelper.EXPECTED.clear();
+		// this can be replaced by @DefaultLocale in JUnit 6.1.0
+		Locale prev = Locale.getDefault();
+		try {
+			Locale.setDefault(Locale.GERMAN);
 
-		assertEquals(
-				", 0:0%, 1:0%, 2:0%, 3:0%, 4:0%, 5:0%, 6:0%, 7:0%, 8:0%, 9:0%, 10:0%, 11:0%, 12:0%, 13:0%, 14:0%, 15:0%, 16:0%, 17:0%, 18:0%",
-				CreateTileOverlaysHelper.concatProgress());
+			CreateTileOverlaysHelper.ACTUAL.clear();
+			CreateTileOverlaysHelper.EXPECTED.clear();
 
-		CreateTileOverlaysHelper.EXPECTED.add(10, 2349);
-		assertEquals(
-				", 0:0%, 1:0%, 2:0%, 3:0%, 4:0%, 5:0%, 6:0%, 7:0%, 8:0%, 9:0%, 10:0,00%, 11:0%, 12:0%, 13:0%, 14:0%, 15:0%, 16:0%, 17:0%, 18:0%",
-				CreateTileOverlaysHelper.concatProgress());
+			assertEquals(
+					", 0:0%, 1:0%, 2:0%, 3:0%, 4:0%, 5:0%, 6:0%, 7:0%, 8:0%, 9:0%, 10:0%, 11:0%, 12:0%, 13:0%, 14:0%, 15:0%, 16:0%, 17:0%, 18:0%",
+					CreateTileOverlaysHelper.concatProgress());
 
-		CreateTileOverlaysHelper.ACTUAL.add(10, 1623);
-		assertEquals(
-				", 0:0%, 1:0%, 2:0%, 3:0%, 4:0%, 5:0%, 6:0%, 7:0%, 8:0%, 9:0%, 10:69%, 11:0%, 12:0%, 13:0%, 14:0%, 15:0%, 16:0%, 17:0%, 18:0%",
-				CreateTileOverlaysHelper.concatProgress());
+			CreateTileOverlaysHelper.EXPECTED.add(10, 2349);
+			assertEquals(
+					", 0:0%, 1:0%, 2:0%, 3:0%, 4:0%, 5:0%, 6:0%, 7:0%, 8:0%, 9:0%, 10:0,00%, 11:0%, 12:0%, 13:0%, 14:0%, 15:0%, 16:0%, 17:0%, 18:0%",
+					CreateTileOverlaysHelper.concatProgress());
 
-		CreateTileOverlaysHelper.ACTUAL.add(11, -1);
-		assertEquals(
-				", 0:0%, 1:0%, 2:0%, 3:0%, 4:0%, 5:0%, 6:0%, 7:0%, 8:0%, 9:0%, 10:69%, 11:_, 12:0%, 13:0%, 14:0%, 15:0%, 16:0%, 17:0%, 18:0%",
-				CreateTileOverlaysHelper.concatProgress());
+			CreateTileOverlaysHelper.ACTUAL.add(10, 1623);
+			assertEquals(
+					", 0:0%, 1:0%, 2:0%, 3:0%, 4:0%, 5:0%, 6:0%, 7:0%, 8:0%, 9:0%, 10:69%, 11:0%, 12:0%, 13:0%, 14:0%, 15:0%, 16:0%, 17:0%, 18:0%",
+					CreateTileOverlaysHelper.concatProgress());
 
-		CreateTileOverlaysHelper.EXPECTED.add(12, 123);
-		CreateTileOverlaysHelper.ACTUAL.add(12, 123);
-		assertEquals(
-				", 0:0%, 1:0%, 2:0%, 3:0%, 4:0%, 5:0%, 6:0%, 7:0%, 8:0%, 9:0%, 10:69%, 11:_, 13:0%, 14:0%, 15:0%, 16:0%, 17:0%, 18:0%",
-				CreateTileOverlaysHelper.concatProgress());
+			CreateTileOverlaysHelper.ACTUAL.add(11, -1);
+			assertEquals(
+					", 0:0%, 1:0%, 2:0%, 3:0%, 4:0%, 5:0%, 6:0%, 7:0%, 8:0%, 9:0%, 10:69%, 11:_, 12:0%, 13:0%, 14:0%, 15:0%, 16:0%, 17:0%, 18:0%",
+					CreateTileOverlaysHelper.concatProgress());
+
+			CreateTileOverlaysHelper.EXPECTED.add(12, 123);
+			CreateTileOverlaysHelper.ACTUAL.add(12, 123);
+			assertEquals(
+					", 0:0%, 1:0%, 2:0%, 3:0%, 4:0%, 5:0%, 6:0%, 7:0%, 8:0%, 9:0%, 10:69%, 11:_, 13:0%, 14:0%, 15:0%, 16:0%, 17:0%, 18:0%",
+					CreateTileOverlaysHelper.concatProgress());
+		} finally {
+			Locale.setDefault(prev);
+		}
 	}
 }
